25928ebad109a7ddf242613e695656b3c4528d92
Benjamin Geyer <brgeyer49@gmail.com>
2019-04-26 16:29:44 -0400

    enabled css modules and sass

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import {Link} from 'gatsby'
 
+//import '../styles/components/header.module.scss'
+import headerStyles from '../styles/components/header.module.scss'
+
 const Header = () => {
   return (
-    <header>
-      <h1>Gatsby Practice</h1>
+    <header className={headerStyles.header}>
+      <h1><Link className={headerStyles.title} to="/">Gatsby Practice</Link></h1>
       <div>
-        <nav><ul>{['Index','Blog','About','Contact'].map(linkGenerator)}</ul></nav>
+        <nav>
+          <ul className={headerStyles.navList}>
+            {['Index','Blog','About','Contact'].map(linkGenerator)}
+          </ul>
+        </nav>
       </div>
     </header>
   )
 }
 
 const linkGenerator = (title, index) => {
-  return title!=='Index' ? (<li key={index}><Link to={'/'+title.toLowerCase()}>{title}</Link></li>) :
-  (<li key={index}><Link to='/'>Home  </Link></li>)
+  return title!=='Index' ? (<li key={index}><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to={'/'+title.toLowerCase()}>{title}</Link></li>) :
+  (<li key={index}><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home  </Link></li>)
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 import Header from './header'
 import Footer from './footer'
+
 //Entry point for CSS and styling
 import '../styles/index.scss'
+import layoutStyles from '../styles/components/layout.module.scss'
 
 const Layout = (props) => {
   return (
-    <div>
-      <Header/>
-      {props.children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header/>
+        {props.children}
+      </div>
       <Footer/>
     </div>
   )
