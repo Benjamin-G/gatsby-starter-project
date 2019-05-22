a016c4f7e42fd7270562f17a371dca6f10f922aa
Benjamin Geyer <brgeyer49@gmail.com>
2019-05-22 16:31:58 -0400

    needed photo support and better styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {graphql, useStaticQuery} from 'gatsby'
+import footerStyles from '../styles/components/footer.module.scss'
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
     query{site{siteMetadata{author}}}
   `)
   return (
-    <footer>
+    <footer className={footerStyles.footer}>
       <p>Created by {data.site.siteMetadata.author}, © 2019</p>
     </footer>
   )
